c6ba4723ca557a35ffe45cde10ab05ded2ca1d04
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-02-25 00:37:43 +0530

    added tooltip project

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,31 @@ import Tooltip from "./Tooltip";
 function App() {
   return (
     <div className="App">
-      <Tooltip />
+      <h1>React Tooltip</h1>
+
+      <div className="example-wrapper">
+        <Tooltip direction="right">
+          <p>RIGHT</p>
+        </Tooltip>
+      </div>
+
+      <div className="example-wrapper">
+        <Tooltip direction="top">
+          <p>TOP</p>
+        </Tooltip>
+      </div>
+
+      <div className="example-wrapper">
+        <Tooltip direction="bottom">
+          <p>BOTTOM</p>
+        </Tooltip>
+      </div>
+
+      <div className="example-wrapper">
+        <Tooltip direction="left" delay="0">
+          <p>LEFT</p>
+        </Tooltip>
+      </div>
     </div>
   );
 }
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,16 +1,38 @@
-import styles from "../css/tooltip.module.css";
+import React, { useState } from "react";
+import "../css/tooltip.css";
 
-function Tooltip() {
-  const handleHover = () => {
-    console.log("Hello");
+const Tooltip = (props) => {
+  let timeout;
+  const [active, setActive] = useState(false);
+
+  const showTip = () => {
+    timeout = setTimeout(() => {
+      setActive(true);
+    }, props.delay || 400);
   };
+
+  const hideTip = () => {
+    clearInterval(timeout);
+    setActive(false);
+  };
+
   return (
-    <div className={styles.tooltip}>
-      <p className={styles.tooltipText} onMouseEnter={handleHover}>
-        Hover over me!
-      </p>
+    <div
+      className="Tooltip-Wrapper"
+      // When to show the tooltip
+      onMouseEnter={showTip}
+      onMouseLeave={hideTip}
+    >
+      {/* Wrapping */}
+      {props.children}
+      {active && (
+        <div className={`Tooltip-Tip ${props.direction || "top"}`}>
+          {/* Content */}
+          <p>This is a tooltip!</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default Tooltip;
